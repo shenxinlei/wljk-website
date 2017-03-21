714fbf8638fb27fd242b2d379468142cfaf86574
Zhenghao Jia <cherie-xia@hotmail.com>
2017-03-21 12:25:01 +0800

    wljk-1.3.21
    dzbHistory UI completed

--- web/jsonce/dzbHistory.js
+++ web/jsonce/dzbHistory.js
@@ -29,111 +29,117 @@ function docReady() {
         },
     });
 
-    /***
-        volte kpi
-    ***/
-    //volte 查询按钮
-    $("#volte-inquire").on("click", function() {
-        $("#volte-table").show();
-        $("#volte-save").show();
+    //时间段自定义选项
+    $("#range").change(function(){ 
+        var p1 = $(this).children('option:selected').val();//这就是selected的值 
+        if (p1 == 7) {
+            $(".form1-range").show();
+        }
+        else {
+            $(".form1-range").hide();
+        }
+    }); 
+
+    //在线查询按钮
+    $("#inquire").on("click", function() {
         //ajax去后台查询，查询到了结果之后，传回到前端
-        $("#volte-form").ajaxSubmit({
+        $("#form1").ajaxSubmit({
             type: "post",
             data: $(this).formSerialize(),
             success: function(responseText, statusText) {
                 //
             }
         });
+        //查询出结果以后就显示到前端
+        $("#kpiOverall").show();
+        
         return false;
     });
-    // volte 保存大值班按钮
-    $("#volte-save").on("click", function() {
-        var judge = true;
-        $("#volte-table input").each(function(){
-            //先判断是否所有值为非空，如果有格子空着是不能保存的
-            if (!$(this).val()) {
-                $.alert({
-                    title: '表中有空的格子没填',
-                    content: ''
-                });
-                judge = false;
-                return false;
-            }
-            //再判断是否是红色的非法数字，如果都不是红色的，说明就可以提交了
-            if ($(this).css("color") == "rgb(255, 0, 0)") {
-                $.alert({
-                    title: '表中有红色非法的数据，请重新填写正确的数据',
-                    content: ''
-                });
-                judge = false;
-                return false;
-            }
-        });
-        if (!judge) {
-            return false;
-        }
-        //到了这一步就可以ajax提交数据到数据库了，这里用alert代替了
-        $.alert({
-            title: '保存成功',
-            content: ''
-        });
-        //最后每次保存成功以后，把保存按钮禁用
-        $("#volte-save").attr("disabled", true);
-    });
-    //volte中 每次input有变动时，保存大值班应该就重新激活
-    $('#volte-table input').change(function() {
-        var input = $(this);  
-        var temp = $.trim(input.val());
-        var value = parseFloat(temp);
-        if (!value) {  
-            $.alert({
-                title: '请输入一个数字',
-                content: ''
-            });
-            input.css("color", "red");  
-            $("#volte-save").attr("disabled", true);
-        }  
-        else {
-            //转化为2位数字
-            input.val(value.toFixed(2));
-            input.css("color", "#3D9140");
-            $("#volte-save").attr("disabled", false);
-        }
+
+    //导出按钮
+    $("#derive").on("click", function() {
+        //直接去后端生成一个excel，根据用户选择的日期和KPI类型
+        //我用Unix时间戳生成一个随机数字，保证excel文件名不重复
+        var date = new Date();
+        var s = date.getTime();
+        //下面的href就是生成的excel存放的相对路径
+        $("#form1").append("<br><a class='btn btn-alert' href='./tables/新监控大值班报表.xls'><i class='glyphicon glyphicon-folder-open icon-white'></i> 大值班KPI-" + s + ".xls</a>");
+        
     });
 
-    $('.form_datetime').datetimepicker({
-        //language:  'fr',
-        weekStart: 1,
-        todayBtn:  1,
-        autoclose: 1,
-        todayHighlight: 1,
-        startView: 2,
-        forceParse: 0,
-        showMeridian: 1
-    });
+    //日期选择期初始化
     $('.form_date').datetimepicker({
-        language:  'fr',
-        weekStart: 1,
+        format:'yyyy-mm-dd',
+        language:  'zh-CN',
+        weekStart: 0,
         todayBtn:  1,
         autoclose: 1,
         todayHighlight: 1,
-        startView: 2,
-        minView: 2,
+        startView: 2,//2代表月份
+        minView: 2,//2代表月份
         forceParse: 0
     });
-    $('.form_time').datetimepicker({
-        language:  'fr',
-        weekStart: 1,
-        todayBtn:  1,
-        autoclose: 1,
-        todayHighlight: 1,
-        startView: 1,
-        minView: 0,
-        maxView: 1,
-        forceParse: 0
+    
+    //书签初始化函数
+    $('#myTab a:first').tab('show');
+    $('#myTab a').click(function (e) {
+        e.preventDefault();
+        $(this).tab('show');
     });
-    console.log("v22");
 
+
+    //原因分析按钮 cmnet
+    $("#btn-cmnet-20170301").click(function (e) {
+        $.alert({
+            title: '原因分析',
+            content: "无"
+        });
+    });
+    $("#btn-cmnet-20170302").click(function (e) {
+        $.alert({
+            title: '原因分析',
+            content: "无"
+        });
+    });
+    $("#btn-cmnet-20170303").click(function (e) {
+        $.alert({
+            title: '原因分析',
+            content: "无"
+        });
+    });
+    $("#btn-cmnet-20170304").click(function (e) {
+        $.alert({
+            title: '原因分析',
+            content: "无"
+        });
+    });
+    $("#btn-cmnet-20170305").click(function (e) {
+        $.alert({
+            title: '原因分析',
+            content: "上海网络运行正常，无影响业务的故障发生。<br>1、CMNET地市入流量为763.55TB，与上月周日参考值661.01 TB比较，增加102.54TB，增幅15.51%。较前一天增加44.21TB，增幅6.15%，较上周同期增加15.73TB，增幅2.10%。主要是由于上月春节大量用户返乡，参考值偏低，本月用户回归正常导致业务量超标。"
+        });
+    });
+
+    //原因分析按钮 volte
+    $("#btn-volte-20170301").click(function (e) {
+        $.alert({
+            title: '原因分析',
+            content: '上海网络运行正常，无影响业务的故障发生。<br>1、VoLTE注册用户数为107.95万户，与上月工作日参考值80.23万户比较，增加27.72万户，增幅34.55%。较前一天减少0.65万户，降幅0.60%，较上周同期增加14.97万户，增幅16.10%。主要是由于上月春节大量用户返乡，参考值偏低，本月用户回归正常导致业务量超标。<br>2、VoLTE话务量为100.26万erl，与上月工作日参考值72.34万erl比较，增加27.92万erl，增幅38.59%。较前一天减少19.13万erl，降幅16.02%，较上周同期增加4.15万erl，增幅4.32%。主要是由于上月春节大量用户返乡，参考值偏低，本月用户回归正常导致业务量超标。'
+        });
+    });
+    $("#btn-volte-20170302").click(function (e) {
+        $.alert({
+            title: '原因分析',
+            content: '上海网络运行正常，无影响业务的故障发生。<br>1、VoLTE注册用户数为106.30万户，与上月工作日参考值80.23万户比较，增加26.07万户，增幅32.50%。较前一天减少1.65万户，降幅1.53%，较上周同期增加10.16万户，增幅10.57%。主要是由于上月春节大量用户返乡，参考值偏低，本月用户回归正常导致业务量超标。<br>2、VoLTE话务量为114.78万erl，与上月工作日参考值72.34万erl比较，增加42.44万erl，增幅58.66%。较前一天增加14.52万erl，增幅14.48%，较上周同期增加14.86万erl，增幅14.87%。主要是由于上月春节大量用户返乡，参考值偏低，本月用户回归正常导致业务量超标。'
+        });
+    });
+    $("#btn-volte-20170303").click(function (e) {
+        $.alert({
+            title: '原因分析',
+            content: "上海网络运行正常，无影响业务的故障发生。<br>1、VoLTE注册用户数为107.22万户，与上月工作日参考值80.23万户比较，增加26.99万户，增幅33.64%。较前一天增加0.92万户，增幅0.86%，较上周同期增加7.76万户，增幅7.80%。主要是由于上月春节大量用户返乡，参考值偏低，本月用户回归正常导致业务量超标。<br>2、VoLTE话务量为116.07万erl，与上月工作日参考值72.34万erl比较，增加43.73万erl，增幅60.44%。较前一天增加1.29万erl，增幅1.12%，较上周同期增加11.54万erl，增幅11.04%。主要是由于上月春节大量用户返乡，参考值偏低，本月用户回归正常导致业务量超标。"
+        });
+    });
+    console.log("v26");
 }
 
 
